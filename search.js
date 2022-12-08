621dd9a27401c6ed86c4c86a305d569836b28bc6
ZutJoe <zhout50@163.com>
2022-12-08 22:39:02 +0800

    chore: 加载和处理data.json文件中的数据

--- search.js
+++ search.js
@@ -1,23 +1,26 @@
-let arrs = ['a', 'aa', 'bbbb', 'aaaa', 'cccc', 'dddd', 'aavv', '你好', '你']
-
 let search_input = document.getElementById('fact_search_input')
 
 function createDivNode () {
     const node = document.createElement('div')
     node.className = 'list-group'
     node.id = 'search_result'
-
     let modal_body = document.getElementById('modal-body')
     modal_body.appendChild(node)
 }
 
-function createANode (content) {
+function createANode (aid, content) {
     const node = document.createElement("a")
     node.className = 'list-group-item list-group-item-action'
-    node.href = '#'
+    node.id = '#' + aid
+    node.href = '#' + aid
     const textnode = document.createTextNode(content)
     node.appendChild(textnode)
     search_result.appendChild(node)
+
+    node.onclick = () => {
+        let search_modal = document.getElementById('search_modal')
+        search_modal.modal('hide')
+    }
 }
 
 search_input.addEventListener('input', (e) => {
@@ -29,12 +32,20 @@ search_input.addEventListener('input', (e) => {
 
     clear()
     createDivNode()
-    for (let i = 0; i < arrs.length; i++) {
-        if (arrs[i].indexOf(search_input.value) !== -1) {
-            createANode(arrs[i])
+    searchAndCreateResultEl(search_input.value)
+})
+
+function searchAndCreateResultEl (msg) {
+    for (let i = 0; i < data_arr.length; i++) {
+        let introduces = data_arr[i].introduces
+        for (let j = 0; j < introduces.length; j ++) {
+            
+            if (introduces[j].indexOf(msg) !== -1) {
+                createANode(data_arr[i].aid, introduces[j])
+            }
         }
     }
-})
+}
 
 function clear() {
     let search_result = document.getElementById('search_result')
